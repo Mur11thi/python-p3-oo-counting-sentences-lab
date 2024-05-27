da94c4f595ffc6d8790fac3cd1551ea78d05f580
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-05-27 12:31:43 +0300

    submits sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self,value =""):
+    self._value = value
+  def get_string(self):
+     return self._value 
+  def set_string(self,value):
+    if (type(value) == str):
+       self._value = value
+    else: 
+      print("The value must be a string.")
+
+  value=property(get_string ,set_string)
+
+  def is_sentence (self):
+      return self._value.endswith(".")
+  
+  def is_question(self):
+     return self._value.endswith("?")
+  
+  def is_exclamation(self):
+     return self._value.endswith("!")
+  def count_sentences(self):
+      value = self._value
+      for punc in ['?','!']:
+        value = value.replace(punc , '.')
+      sentences= [s for s in value.split('.') if s] 
+
+
+      # sentences = [s for s in value.split('.') if s]    
+      return len(sentences)
+
+string = MyString("DAS")  
+
+
+      
